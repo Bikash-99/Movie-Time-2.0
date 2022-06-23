e3e22af699beecb79ce94a63465fee73792f064a
Bikash Maity <107612506+Bikash-99@users.noreply.github.com>
2022-06-23 09:45:31 +0530

    Update callback.py

--- LuciferMoringstar_Robot/callback.py
+++ LuciferMoringstar_Robot/callback.py
@@ -231,8 +231,8 @@ async def cb_handler(bot, update):
 
             FILE_CAPTION = settings["caption"]
             caption = FILE_CAPTION.format(mention=update.from_user.mention, file_name=title, size=size, caption=files.caption)
-            buttons = [[ InlineKeyboardButton(f"🆘👤 Owner", url="https://t.me/hellodarklord"), InlineKeyboardButton(f"🆘🤖 Contact", url="https://t.me/Panchmisali_2_Bot")
-                       [  InlinekeyboardButton("❌ Close", callback_data="close"), ]]
+            buttons = [[ InlineKeyboardButton(f"🆘👤 Owner", url="https://t.me/hellodarklord"), InlineKeyboardButton(f"🆘🤖 Contact", url="https://t.me/hellodarklord"),
+                      [  InlinekeyboardButton("❌ Close", callback_data="close") ]]
                if settings["savefiles"]:
                 protect_content = True
             else:
@@ -293,8 +293,8 @@ async def cb_handler(bot, update):
             title = files.file_name
             size = get_size(files.file_size)
             caption = CUSTOM_FILE_CAPTION.format(mention=update.from_user.mention, file_name=title, size=size, caption=files.caption)
-            buttons = [[ InlineKeyboardButton(f"🆘👤 Owner", url="https://t.me/hellodarklord"), InlineKeyboardButton(f"🆘🤖 Contact", url="https://t.me/Panchmisali_2_Bot")
-                      [  InlinekeyboardButton("❌ Close", callback_data="close"), ]]
+            buttons = [[ InlineKeyboardButton(f"🆘👤 Owner", url="https://t.me/hellodarklord"), InlineKeyboardButton(f"🆘🤖 Contact", url="https://t.me/hellodarklord"),
+                      [  InlinekeyboardButton("❌ Close", callback_data="close") ]]
             try:
                 await bot.send_cached_media(chat_id=update.from_user.id, file_id=file_id, caption=caption, reply_markup=InlineKeyboardMarkup(buttons), protect_content=SAVE_FILES)            
             except Exception as e:
